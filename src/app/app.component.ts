6cee3696902532e14049d5f10ed81ec36484692c
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2024-01-04 18:46:54 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pass-checker-angular';
+  password: string = '';
+
+  get passwordStrength(): string {
+    return this.getPasswordStrength();
+  }
+
+  getPasswordStrength(): string {
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasDigits = /\d/.test(this.password);
+    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(this.password);
+
+    if (this.password.length >= 8 && hasLetters && hasDigits && hasSymbols) {
+      return 'strong';
+    } else if (this.password.length >= 8 && ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols))) {
+      return 'medium';
+    } else if (this.password.length >= 8 && (hasLetters || hasSymbols || hasDigits)) {
+      return 'easy';
+    } else {
+      return 'weak';
+    }
+  }
 }
